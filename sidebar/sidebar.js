accaae8d630122dbb8c46ac51c3002075e8491db
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-20 13:16:11 +0100

    Feat: format javascript file

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,37 +1,41 @@
 // sidebar class
 
 class SideBar {
-    constructor(menu) {
-        this.menu = menu;
-        this.sideMenu = document.querySelector('.side-bar');
-        this.sideMenuIcon = document.querySelector('.sidemenu-bar');
-        this.logo = document.querySelector('.logo');
+  constructor(menu) {
+    this.menu = menu;
+    this.sideMenu = document.querySelector(".side-bar");
+    this.sideMenuIcon = document.querySelector(".sidemenu-bar");
+    this.logo = document.querySelector(".logo");
 
-        this.menu.addEventListener('click', () => {
-            this.toggleMenu();
-        });
-        this.isOpen = false;
-    }
+    this.menu.addEventListener("click", () => {
+      this.toggleMenu();
+    });
+    this.isOpen = false;
+  }
 
-    toggleMenu() {
-        this.isOpen = !this.isOpen;
-        if(this.isOpen) {
-            TweenMax.to(this.sideMenu, 1, {width: "100%", onComplete:() => {
-                this.sideMenuIcon.style.color = "white";
-                this.sideMenuIcon.classList.replace('fa-bars', 'fa-times');
-                this.logo.style.color = "white";
-            }});
-        } else {
-            TweenMax.to(this.sideMenu, 1, {width: "0%", onComplete:() => {
-                this.sideMenuIcon.style.color = "black";
-                this.sideMenuIcon.classList.replace('fa-times', 'fa-bars');
-                this.logo.style.color = "black";
-            }});
-            
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+    if (this.isOpen) {
+      TweenMax.to(this.sideMenu, 1, {
+        width: "100%",
+        onComplete: () => {
+          this.sideMenuIcon.style.color = "white";
+          this.sideMenuIcon.classList.replace("fa-bars", "fa-times");
+          this.logo.style.color = "white";
         }
-        
+      });
+    } else {
+      TweenMax.to(this.sideMenu, 1, {
+        width: "0%",
+        onComplete: () => {
+          this.sideMenuIcon.style.color = "black";
+          this.sideMenuIcon.classList.replace("fa-times", "fa-bars");
+          this.logo.style.color = "black";
+        }
+      });
     }
+  }
 }
 
-const sidemenuElement = document.querySelector('.sidemenu-bar');
+const sidemenuElement = document.querySelector(".sidemenu-bar");
 const sideBar = new SideBar(sidemenuElement);
