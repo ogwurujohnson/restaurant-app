87be97b7569c4ec90ea3f8a119920d9e56421055
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-20 12:41:26 +0100

    Feat: Style sidebar to change icon and icon color when clicked

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -4,7 +4,9 @@ class SideBar {
     constructor(menu) {
         this.menu = menu;
         this.sideMenu = document.querySelector('.side-bar');
-        
+        this.sideMenuIcon = document.querySelector('.sidemenu-bar');
+        this.logo = document.querySelector('.logo')
+
         this.menu.addEventListener('click', () => {
             this.toggleMenu();
         })
@@ -15,7 +17,11 @@ class SideBar {
         this.isOpen = !this.isOpen;
         if(this.isOpen) {
             this.sideMenu.style.width = "100%";
+            this.sideMenuIcon.style.color = "white";
+            this.sideMenuIcon.classList.replace('fa-bars', 'fa-times');
         } else {
+            this.sideMenuIcon.style.color = "black";
+            this.sideMenuIcon.classList.replace('fa-times', 'fa-bars');
             this.sideMenu.style.width = "0%";
         }
         
