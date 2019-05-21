875faf65693f2a4074fa9b2ba54965a9945e2e8a
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-21 03:18:14 +0100

    Feat: Style the search area

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -4,6 +4,7 @@ class SideBar {
   constructor(menu) {
     this.menu = menu;
     this.logo = document.querySelector(".logo");
+    this.mainHeader = document.querySelector(".main-header");
 
     window.addEventListener('load', () => {
       TweenMax.from(this.logo, 1, { ease: Circ.easeIn, x: -200  });
@@ -30,15 +31,17 @@ class SideBar {
           this.sideMenuIcon.style.color = "white";
           this.sideMenuIcon.classList.replace("fa-bars", "fa-times");
           this.logo.style.color = "white";
+          this.mainHeader.style.backgroundColor = "black";
         }
       });
     } else {
       TweenMax.to(this.sideMenu, 1, {
         width: "0%",
         onComplete: () => {
-          this.sideMenuIcon.style.color = "black";
+          this.sideMenuIcon.style.color = "white";
           this.sideMenuIcon.classList.replace("fa-times", "fa-bars");
-          this.logo.style.color = "black";
+          this.logo.style.color = "white";
+          this.mainHeader.style.backgroundColor = "#6200EE";
         }
       });
     }
